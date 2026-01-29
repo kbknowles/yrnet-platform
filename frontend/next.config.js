024b0fbeb591eef0c1633c269570e66f6caeb821
Kristina Knowles <kriskno@gmail.com>
2026-01-29 11:17:06 -0600

    fix typo in next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "ahsra.onrender.com",
+        hostname: "ahsra-api.onrender.com",
         pathname: "/uploads/**",
       },
     ],
