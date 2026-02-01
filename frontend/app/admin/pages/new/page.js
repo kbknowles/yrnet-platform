57a5a8d523d03d6ba3a240d46778ecff875fedbe
Kristina Knowles <kriskno@gmail.com>
2026-02-01 10:19:14 -0600

    fixed typo

--- frontend/app/admin/pages/new/page.js
+++ frontend/app/admin/pages/new/page.js
@@ -4,7 +4,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import PageEditor from "../PageEditor_tmp";
+import PageEditor from "../PageEditor";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
