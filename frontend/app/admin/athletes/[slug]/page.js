5045921369d67d68ddc3adb8fda5c1c21f20b58b
Kristina Knowles <kriskno@gmail.com>
2026-02-13 12:40:18 -0600

    add sponsors to athletes

--- frontend/app/admin/athletes/[slug]/page.js
+++ frontend/app/admin/athletes/[slug]/page.js
@@ -4,7 +4,16 @@ import { useParams } from "next/navigation";
 import AthleteForm from "../AthleteForm";
 
 export default function EditAthletePage() {
-  const { slug } = useParams();
+  const params = useParams();
+  const slug = params?.slug;
+
+  if (!slug) {
+    return (
+      <div className="p-6">
+        Loading...
+      </div>
+    );
+  }
 
   return <AthleteForm slug={slug} mode="edit" />;
 }
