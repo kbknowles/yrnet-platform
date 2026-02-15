3bd23574e1cad9da9da23e9821332f008682d647
Kristina Knowles <kriskno@gmail.com>
2026-02-15 10:32:01 -0600

    changed background to white

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         className={`${firaSans.variable} min-h-screen flex flex-col`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 bg-white">{children}</main>
         <Footer />
       </body>
     </html>
